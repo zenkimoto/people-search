10c08a6a29964a2150138c24f1a1b748db01810a
Alex Yip <alex.yip@adhocteam.us>
2021-05-19 09:47:13 -0700

    Added formik and yup

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,42 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{ name: '' }}
+        validationSchema={Yup.object({
+          name: Yup.string().required('Required'),
+        })}
+      >
+        <Form>
+          <div class="relative">
+            <svg
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              />
+            </svg>
+            <Field
+              className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10"
+              name="name"
+              type="text"
+              placeholder="Search Name"
+            />
+          </div>
+          <ErrorMessage name="name" />
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default Search;
