0e7be917b54222aeb8aca164b2f19bb02cd9a514
Alex Yip <alex.yip@adhocteam.us>
2021-05-19 08:43:04 -0700

    Added search and results components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Results from './components/results/results';
+import Search from './components/search/search';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Search />
+      <Results />
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders the App component correctly', () => {
   render(<App />);
-  const textElement = screen.getByText(/App/i);
+  const textElement = screen.getByText(/Search/i);
   expect(textElement).toBeInTheDocument();
 });
