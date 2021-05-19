dd015083041b5d561022108a9465918be36db022
Alex Yip <alex.yip@adhocteam.us>
2021-05-19 08:25:39 -0700

    Removed template code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the App component correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/App/i);
+  expect(textElement).toBeInTheDocument();
 });
